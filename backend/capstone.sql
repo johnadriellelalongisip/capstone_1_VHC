364f673e3891ea00c90fde1e9cac708900d0ff8b
ampelme <olajohnfilhmar@gmail.com>
2024-04-15 06:55:12 +0800

    update database

--- backend/capstone.sql
+++ backend/capstone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 03, 2024 at 04:43 AM
+-- Generation Time: Apr 14, 2024 at 10:54 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -44,7 +44,7 @@ CREATE TABLE `medicalstaff` (
 --
 
 INSERT INTO `medicalstaff` (`staff_id`, `staff_username`, `staff_password`, `staff_email`, `staff_role`, `account_created_at`, `account_last_updated_at`, `staff_last_activity`, `staff_history`) VALUES
-(24, 'asdfasdf', '$2b$10$GGSae1g9bGYbAS67GkR25.0bMyIBEuGxx8bo.d.VBTqhuaiiNsrL6', 'asdfasdf@asdf', 'USER', '2024-02-17 13:05:01', '2024-02-17 13:05:01', '2024-02-17 13:05:01', '{\"account_created_at\": \"2024-02-17 13:05:01\", \"2024-02-17 21:17:41\": \"Logged In\", \"2024-03-26 19:35:56\": \"Logged In\", \"2024-03-26 19:36:06\": \"Logged In\", \"2024-03-26 19:36:08\": \"Logged In\", \"2024-03-26 20:10:27\": \"Logged In\", \"2024-03-26 20:40:52\": \"Logged In\", \"2024-03-27 07:05:14\": \"Logged In\", \"2024-03-27 07:07:43\": \"Logged In\", \"2024-03-27 07:26:16\": \"Logged In\", \"2024-03-27 07:28:58\": \"Logged In\", \"2024-03-27 07:29:28\": \"Logged In\", \"2024-03-27 07:30:20\": \"Logged In\", \"2024-03-27 07:30:40\": \"Logged In\", \"2024-03-27 07:31:02\": \"Logged In\", \"2024-03-29 10:19:57\": \"Logged In\", \"2024-03-29 10:31:03\": \"Logged In\", \"2024-03-29 10:43:29\": \"Logged In\", \"2024-03-29 10:45:06\": \"Logged In\", \"2024-03-29 11:04:55\": \"Logged In\", \"2024-03-29 11:10:20\": \"Logged In\", \"2024-03-29 11:11:22\": \"Logged In\", \"2024-03-29 11:34:47\": \"Logged In\", \"2024-03-29 11:35:28\": \"Logged In\", \"2024-03-29 11:36:36\": \"Logged In\", \"2024-03-29 12:36:54\": \"Logged In\", \"2024-03-29 13:13:55\": \"Logged In\", \"2024-03-29 13:14:16\": \"Logged In\", \"2024-03-29 13:44:35\": \"Logged In\", \"2024-03-29 13:44:58\": \"Logged In\", \"2024-03-29 13:48:35\": \"Logged In\", \"2024-03-29 13:51:14\": \"Logged In\", \"2024-03-29 14:12:04\": \"Logged In\", \"2024-03-29 14:13:51\": \"Logged In\", \"2024-03-29 15:50:33\": \"Logged In\", \"2024-03-29 15:52:50\": \"Logged In\", \"2024-03-29 15:53:15\": \"Logged In\", \"2024-03-29 15:54:05\": \"Logged In\"}'),
+(24, 'asdfasdf', '$2b$10$GGSae1g9bGYbAS67GkR25.0bMyIBEuGxx8bo.d.VBTqhuaiiNsrL6', 'asdfasdf@asdf', 'USER', '2024-02-17 13:05:01', '2024-02-17 13:05:01', '2024-02-17 13:05:01', '{\"account_created_at\": \"2024-02-17 13:05:01\", \"2024-02-17 21:17:41\": \"Logged In\", \"2024-03-26 19:35:56\": \"Logged In\", \"2024-03-26 19:36:06\": \"Logged In\", \"2024-03-26 19:36:08\": \"Logged In\", \"2024-03-26 20:10:27\": \"Logged In\", \"2024-03-26 20:40:52\": \"Logged In\", \"2024-03-27 07:05:14\": \"Logged In\", \"2024-03-27 07:07:43\": \"Logged In\", \"2024-03-27 07:26:16\": \"Logged In\", \"2024-03-27 07:28:58\": \"Logged In\", \"2024-03-27 07:29:28\": \"Logged In\", \"2024-03-27 07:30:20\": \"Logged In\", \"2024-03-27 07:30:40\": \"Logged In\", \"2024-03-27 07:31:02\": \"Logged In\", \"2024-03-29 10:19:57\": \"Logged In\", \"2024-03-29 10:31:03\": \"Logged In\", \"2024-03-29 10:43:29\": \"Logged In\", \"2024-03-29 10:45:06\": \"Logged In\", \"2024-03-29 11:04:55\": \"Logged In\", \"2024-03-29 11:10:20\": \"Logged In\", \"2024-03-29 11:11:22\": \"Logged In\", \"2024-03-29 11:34:47\": \"Logged In\", \"2024-03-29 11:35:28\": \"Logged In\", \"2024-03-29 11:36:36\": \"Logged In\", \"2024-03-29 12:36:54\": \"Logged In\", \"2024-03-29 13:13:55\": \"Logged In\", \"2024-03-29 13:14:16\": \"Logged In\", \"2024-03-29 13:44:35\": \"Logged In\", \"2024-03-29 13:44:58\": \"Logged In\", \"2024-03-29 13:48:35\": \"Logged In\", \"2024-03-29 13:51:14\": \"Logged In\", \"2024-03-29 14:12:04\": \"Logged In\", \"2024-03-29 14:13:51\": \"Logged In\", \"2024-03-29 15:50:33\": \"Logged In\", \"2024-03-29 15:52:50\": \"Logged In\", \"2024-03-29 15:53:15\": \"Logged In\", \"2024-03-29 15:54:05\": \"Logged In\", \"2024-04-05 10:14:37\": \"Logged In\", \"2024-04-12 17:39:44\": \"Logged In\", \"2024-04-13 12:17:10\": \"Logged In\", \"2024-04-14 07:29:43\": \"Logged In\"}'),
 (25, 'asdfasdf', '$2b$10$cUwKYyQnyof9j830Oa2rF.wNMcUSvE4pMl7/y5CsNN3VjuEZ..n8W', 'asdfasdf@asdf', 'USER', '2024-02-17 21:07:48', '2024-02-17 21:07:48', '2024-02-17 21:07:48', '{\"account_created_at\": \"2024-02-17 21:07:48\"}'),
 (26, 'asdfasdf', '$2b$10$e6wxnD6NDeeldb/pVLQGeOvtkd4eRfvIUNeQu9YdnH16DoFfbWgCu', 'olajohnfilhmar@gmail.com', 'USER', '2024-03-26 19:35:17', '2024-03-26 19:35:17', '2024-03-26 19:35:17', '{\"account_created_at\": \"2024-03-26 19:35:17\"}'),
 (27, 'asdfasdf', '$2b$10$QTJb8w6uI.qvGK6TTHzhZeKLb7FYYS.3bAfSHJsXyy696W1lwfF9y', 'asdf@asdf.com', 'USER', '2024-03-26 19:35:54', '2024-03-26 19:35:54', '2024-03-26 19:35:54', '{\"account_created_at\": \"2024-03-26 19:35:54\"}');
@@ -73,6 +73,20 @@ CREATE TABLE `municipal_citizens` (
 --
 
 INSERT INTO `municipal_citizens` (`citizen_family_id`, `citizen_firstname`, `citizen_middlename`, `citizen_lastname`, `citizen_gender`, `citizen_birthdate`, `citizen_barangay`, `date_added`, `citizen_number`, `citizen_history`) VALUES
+('FAMILY_ID-4ON0EKYR', 'QASDSAD', 'ASDASD', 'ASDASD', 'male', '2001-01-01', 'ASDASD', '2024-04-13 09:53:46', '421421421421', '{\"2024-04-13 09:53:46\": \"Record Added\"}'),
+('FAMILY_ID-7PUJIUBO', 'Jan Ryan', 'Delos Reyes', 'Ola', 'male', '2008-01-27', 'Antonino (Malinao)', '2024-04-09 18:06:26', '910241289412', '{\"2024-04-09 18:06:26\": \"Record Added\"}'),
+('FAMILY_ID-BKOJULBX', 'asdfasdf', 'asdfasdf', 'asdfasdf', 'male', '2001-01-01', 'asdfasdf', '2024-04-13 12:17:35', '666666666666', '{\"2024-04-13 12:17:35\": \"Record Added\"}'),
+('FAMILY_ID-G8UGVFZW', 'qqqqqqqqqqq', 'qqqqqqqqqqq', 'qqqqqqqqqqq', 'male', '2001-01-01', 'qqqqqqqqqqq', '2024-04-12 19:25:24', '555555555555', '{\"2024-04-12 19:25:24\": \"Record Added\"}'),
+('FAMILY_ID-GR2TIQWL', '1111111111111111111111111', '1111111111111111111111111', '1111111111111111111111111', 'male', '2001-01-01', '1111111111111111111111111', '2024-04-13 09:54:04', '111111111111', '{\"2024-04-13 09:54:04\": \"Record Added\"}'),
+('FAMILY_ID-HNTQJ2D0', 'Elizabeth', 'Aquino', 'Geronaga', 'female', '2000-12-31', 'Antonino (Malinao)', '2024-04-09 18:01:34', '09668649640', '{\"2024-04-09 18:01:34\": \"Record Added\"}'),
+('FAMILY_ID-HNZIVKLI', '2222222222222', '2222222222222', '2222222222222', 'male', '2001-01-01', '2222222222222', '2024-04-12 19:09:29', '222222222222', '{\"2024-04-12 19:09:29\": \"Record Added\"}'),
+('FAMILY_ID-JDFBZLR0', 'Joemar', 'Quinto', 'Ola', 'male', '1975-11-02', 'Antonino (Malinao)', '2024-04-09 18:03:37', '091241241241', '{\"2024-04-09 18:03:37\": \"Record Added\"}'),
+('FAMILY_ID-MT1PT2FW', 'qqqqqqqqqqqqq', 'qqqqqqqqqqqqq', 'qqqqqqqqqqqqq', 'male', '2001-01-01', 'qqqqqqqqqqqqq', '2024-04-13 11:19:12', '555555555555', '{\"2024-04-13 11:19:12\": \"Record Added\"}'),
+('FAMILY_ID-OG6QLS0R', '2222222222222', '2222222222222', '2222222222222', 'male', '2001-01-01', '2222222222222', '2024-04-12 19:09:54', '222222222222', '{\"2024-04-12 19:09:54\": \"Record Added\"}'),
+('FAMILY_ID-R1VQU9RU', '5555555555555555', '5555555555555555', '5555555555555555', 'male', '2001-01-01', '5555555555555555', '2024-04-13 07:57:47', '555555555555', '{\"2024-04-13 07:57:47\": \"Record Added\"}'),
+('FAMILY_ID-SSTDIICY', 'JAJAJAJAJA', 'JAJAJAJAJA', 'JAJAJAJAJA', 'male', '2001-01-01', 'JAJAJAJAJA', '2024-04-14 07:29:55', '444444444444', '{\"2024-04-14 07:29:55\": \"Record Added\"}'),
+('FAMILY_ID-TLT8PKZ7', '1111111111111111111111111', '1111111111111111111111111', '1111111111111111111111111', 'male', '2001-01-01', '1111111111111111111111111', '2024-04-13 09:54:19', '111111111111', '{\"2024-04-13 09:54:19\": \"Record Added\"}'),
+('FAMILY_ID-VIEWAG03', '2222222222222', '2222222222222', '2222222222222', 'male', '2001-01-01', '2222222222222', '2024-04-12 19:09:22', '222222222222', '{\"2024-04-12 19:09:22\": \"Record Added\"}'),
 ('FAMILY_ID-WPY8ECKV', 'John Filhmar', 'De Los Reyes', 'Ola', 'male', '2001-02-21', 'Canubing 1', '2024-02-29 20:07:18', '09777353309', '{\"2024-02-29 20:07:18\": \"Record Added\"}'),
 ('FAMILY_ID-YHVNHRJE', 'Jef', 'Donald', 'Ramos', 'male', '2003-05-12', 'Puerto', '2024-02-29 21:03:10', '09128241827', '{\"2024-02-29 21:03:10\": \"Record Added\"}');
 
@@ -102,7 +116,8 @@ INSERT INTO `patient_queue` (`queue_number`, `patient_name`, `patient_gender`, `
 (1, 'Filhmar', 'male', 'Canubing 1', '2024-03-18 12:18:45', NULL, NULL, 'serving', NULL),
 (2, 'Zabeth', 'male', 'Bucayao', '2024-03-18 12:31:12', NULL, NULL, 'serving', NULL),
 (3, 'asdf', 'male', 'asdf', '2024-03-18 18:58:11', NULL, NULL, 'waiting', NULL),
-(4, 'asdffasdf', 'male', 'asdfasd', '2024-04-03 09:44:29', NULL, NULL, 'waiting', NULL);
+(4, 'asdffasdf', 'male', 'asdfasd', '2024-04-03 09:44:29', NULL, NULL, 'waiting', NULL),
+(5, 'asdfdsaf', 'male', 'Poblacion II', '2024-04-12 19:21:56', NULL, NULL, 'waiting', NULL);
 
 -- --------------------------------------------------------
 
@@ -200,7 +215,7 @@ ALTER TABLE `medicalstaff`
 -- AUTO_INCREMENT for table `patient_queue`
 --
 ALTER TABLE `patient_queue`
-  MODIFY `queue_number` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `queue_number` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `pharmacy_inventory`
